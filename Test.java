7d8d154381b6eeeace132f87499822740cf5862a
Kragh <eaa23chkr@students.eaaa.dk>
2024-04-17 09:31:49 +0200

    Dette er endnu et push af Christian

--- Test.java
+++ Test.java
@@ -5,5 +5,7 @@ public class Test {
         System.out.println("Mahad, Johannes, Christian og Kenneth er seje!");
 
         System.out.println("Virker dette? - Chrsitan");
+
+        System.out.println("Hej Kenneth");
     }
 }
